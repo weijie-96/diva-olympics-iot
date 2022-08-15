5b33baf256b93e9c5c5c97e45424855c50f135cb
Edmond Chen Si Jie <edmond.chen@changiairport.com>
2022-08-15 10:44:27 +0800

    Reorganised code

--- etc/apps/diva/bin/deploysample.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-rm -rf /home/pi/diva
-mkdir /home/pi/diva
-cp -rf ./diva /home/pi/
-cp -f ./grovepi.py /home/pi/diva/assignment_3
-sudo chown -R pi: /home/pi/diva
-echo Diva example codes copied to /home/pi/
